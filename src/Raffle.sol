029c92296a1d22954095f46214884bd03eedb67f
Annonnymmousss <jatin02012006@gmail.com>
2025-10-18 04:16:36 +0530

    feat : inilialized raffle contract

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -2,4 +2,30 @@
 
 pragma solidity ^0.8.19;
 
-contract Raffle {}
+
+/**
+ * @title A sample Raffle Contract
+ * @author Patrick Collins (or even better, you own name)
+ * @notice This contract is for creating a sample raffle
+ * @dev It implements Chainlink VRFv2.5 and Chainlink Automation
+ */
+
+
+contract Raffle {
+    uint256 private immutable i_entranceFee;
+    constructor(uint256 entranceFee){
+        i_entranceFee=entranceFee;
+    }
+
+    function enterRaffle() public payable{}
+
+    function pickWinner() public {}
+
+    /**
+     * Getter Functions
+     */
+
+    function getEntranceFee() external view returns (uint256){
+        return i_entranceFee;
+    }
+}
